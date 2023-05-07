0b37318c7844101bd5db7d64105496780da93746
halimajamaleddine <127838079+halimajamaleddine@users.noreply.github.com>
2023-05-07 21:44:21 +0100

    Saerch-form

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import './Search.css'
+
 
 function Search() {
     return (
         <div>
-            <h1>Search</h1>
+            <div className='cont search'>
+                <form className='search-form'>
+                    <div className='search-form-row'>
+                        <h1>Search</h1>
+                    </div>
+                    <div className='search-form-row'>
+                        <label>Prix Maximale</label>
+                        <input type="text" />
+
+                    </div>
+                    <div className='search-form-row'>
+                        <label>Noms Parkings</label>
+                        <select>
+                            <option>parking1</option>
+                            
+                        </select>
+                    </div>
+                    <div className='search-form-row'>
+                    <a className='btn btn-submit-search' href='#'>Submit</a>
+                </div>
+
+                </form>
+            </div>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
-import AddForm from '../components/AddForm/AddForm'
 import Landing from '../components/Landing/Landing'
+import Search from '../components/Search/Search'
 
 function Home() {
     return (
         <div>
             <Navbar/>
             <Landing/>
+            <Search/>
         </div>
     )
 }
