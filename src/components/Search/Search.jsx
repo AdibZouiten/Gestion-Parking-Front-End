acc0a30bb52094a1709d6832c69252c2d9355401
halimajamaleddine <127838079+halimajamaleddine@users.noreply.github.com>
2023-05-07 22:06:21 +0100

    Search-v

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import './Search.css'
-
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 function Search() {
+    const [maxPrice, setMaxPrice] = useState(0);
+    const [parkings, setParkings] = useState([]);
+
+    useEffect(() => {
+        // Fetch parkings data from API
+        const fetchParkings = async () => {
+            try {
+                const response = await fetch('/api/parkings');
+                const data = await response.json();
+                setParkings(data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchParkings();
+    }, []);
+
+    const handlePriceChange = (event) => {
+        setMaxPrice(event.target.value);
+    };
+
+    const filteredParkings = parkings.filter(
+        (parking) => parking.price <= maxPrice
+    );
     return (
         <div>
             <div className='cont search'>
@@ -12,19 +38,23 @@ function Search() {
                     </div>
                     <div className='search-form-row'>
                         <label>Prix Maximale</label>
-                        <input type="text" />
+                        <input type="text" value={maxPrice} onChange={handlePriceChange} />
 
                     </div>
                     <div className='search-form-row'>
-                        <label>Noms Parkings</label>
-                        <select>
-                            <option>parking1</option>
-                            
-                        </select>
+                        <ul>
+                            {filteredParkings.map((parking) => (
+                                <li key={parking.id}>
+                                    <Link to={`/parkings/${parking.id}`}>
+                                        {parking.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                     <div className='search-form-row'>
-                    <a className='btn btn-submit-search' href='#'>Submit</a>
-                </div>
+                        <a className='btn btn-submit-search' href='#'>Submit</a>
+                    </div>
 
                 </form>
             </div>
